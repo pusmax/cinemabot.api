da883fc6e838965a0933f6b67528915c63ecdbcc
pusmax <pumaks999@gmail.com>
2018-12-18 15:25:26 +0200

    resolve merge conflicts

--- src/main/java/dao/metamodel/enteties/Object.java
+++ src/main/java/dao/metamodel/enteties/Object.java
@@ -1,6 +1,14 @@
 package dao.metamodel.enteties;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "objects")
 public class Object {
+    @Id @GeneratedValue
     private int id;
     private String name;
     private ObjectType objectType;
--- src/main/java/dao/metamodel/enteties/Param.java
+++ src/main/java/dao/metamodel/enteties/Param.java
@@ -1,11 +1,18 @@
 package dao.metamodel.enteties;
 
+import javax.persistence.*;
 import java.sql.Date;
 
+@Entity
+@Table(name = "params")
 public class Param {
+    @Id @GeneratedValue
     private int id;
+    @Column(name = "text_value")
     private String textValue;
+    @Column(name = "number_value")
     private int numbValue;
+    @Column(name = "date_value")
     private Date dateValue;
     private Attribute attribute;
     private Object object;
