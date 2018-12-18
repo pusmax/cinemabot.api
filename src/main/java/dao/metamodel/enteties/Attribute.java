d9c5bea631c92143f8f7754b01f67daf117ac7fa
Rostislav <rostik_99@icloud.com>
2018-12-18 15:15:56 +0200

    added metamodel entities

--- src/main/java/dao/metamodel/enteties/Attribute.java
+++ src/main/java/dao/metamodel/enteties/Attribute.java
@@ -1,6 +1,14 @@
 package dao.metamodel.enteties;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "attributes")
 public class Attribute {
+    @Id @GeneratedValue
     private int id;
     private String name;
     private ObjectType objectType;
--- src/main/java/dao/metamodel/enteties/ObjectType.java
+++ src/main/java/dao/metamodel/enteties/ObjectType.java
@@ -1,6 +1,14 @@
 package dao.metamodel.enteties;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "object_type")
 public class ObjectType {
+    @Id @GeneratedValue
     private int id;
     private String name;
 }
